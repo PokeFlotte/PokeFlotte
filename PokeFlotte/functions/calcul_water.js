43f74d82323da2afac8cae843b2e4413f594cdd5
m20007529 <aurelien.mattera@etu.univ-amu.fr>
2023-03-27 11:01:02 +0200

    modif fonction calcul_water

--- PokeFlotte/functions/calcul_water.js
+++ PokeFlotte/functions/calcul_water.js
@@ -10,19 +10,19 @@ function calculWater(weight, nbDefault=8) {
         return 4;
     }
     else if(weight < 54){
-        console.log("4 verres par jour");
+        console.log("6 verres par jour");
         return 6;
     }
      else if(weight<72){
-        console.log("4 verres par jour");
-        nb_glass = 8;
+        console.log("8 verres par jour");
+        return 8;
     }
     else if(weight<90){
-        console.log("4 verres par jour");
+        console.log("11 verres par jour");
         return 11;
     }
     else {
-        console.log("4 verres par jour");
+        console.log("14 verres par jour");
         return 14;
     }
 }
